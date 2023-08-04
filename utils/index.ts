b0d12774b2a82f77b0bc9eb7fd23e1218b63f6fb
ELFATIMI SALAHEDDINE <salahfatimi16@gmail.com>
2023-08-04 13:00:37 +0100

    sort by A to Z

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,18 @@
-
 export async function fetchCountry() {
-    const response= await fetch("https://restcountries.com/v3.1/all")
-    const result= await response.json()
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  const result = await response.json();
+  const sortedArray = result.sort(
+    (a: { name: { common: string } }, b: { name: { common: string } }) => {
+      return a.name.common.toLowerCase() < b.name.common.toLowerCase() ? -1 : 1;
+    }
+  );
 
-    return result
-  
+  return sortedArray;
 }
 
-export async function fetchNAme(name:string) {
-    const response= await fetch(`https://restcountries.com/v3.1/name/${name}`)
-    const result= await response.json()
+export async function fetchNAme(name: string) {
+  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`);
+  const result = await response.json();
 
-    return result
-  
+  return result;
 }
